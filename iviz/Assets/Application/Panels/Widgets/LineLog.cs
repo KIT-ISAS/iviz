a84648f268a460375c6b4e74786da860e7ec6da3
Antonio Zea <akzeac@Chuchico-de-Platino.local>
2020-05-31 13:09:14 +0200

    Adding line log

---
+++ iviz/Assets/Application/Panels/Widgets/LineLog.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iviz.App
+{
+    public class LineLog : Widget
+    {
+        public Text text;
+        public GameObject content;
+
+        readonly List<string> lines = new List<string>();
+        const int maxLines = 100;
+
+        bool active = true;
+        public bool Active
+        {
+            get => active;
+            set
+            {
+                active = value;
+                if (value)
+                {
+                    Flush();
+                }
+            }
+        }
+
+        public void Add(string text)
+        {
+            string[] sublines = text.Split('\n');
+            lines.AddRange(sublines);
+        }
+
+        public void Flush()
+        {
+            if (!Active)
+            {
+                return;
+            }
+            int overflow = lines.Count - maxLines;
+            if (overflow > 0)
+            {
+                lines.RemoveRange(0, overflow);
+            }
+
+            StringBuilder str = new StringBuilder();
+            lines.ForEach(x => str.AppendLine(x));
+
+            text.text = str.ToString();
+            float y = text.preferredHeight;
+
+            RectTransform ctransform = (RectTransform)content.transform;
+            ctransform.sizeDelta = new Vector2(0, y);
+        }
+
+        public override void ClearSubscribers()
+        {
+            Active = false;
+        }
+    }
+}
