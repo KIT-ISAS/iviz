f98f8c75bc3556b54356e12cecc5a079c5dfe02c
Antonio Zea <akzeac@Chuchico-de-Platino.local>
2020-09-02 09:27:24 +0200

    Adding sdf texture

---
+++ iviz_urdf/Sdf/Texture.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Xml;
+using Iviz.Urdf;
+
+namespace Iviz.Sdf
+{
+    public sealed class Texture
+    {
+        public double Size { get; }
+        public string Diffuse { get; }
+        public string Normal { get; }
+
+        internal Texture(XmlNode node)
+        {
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                switch (child.Name)
+                {
+                    case "size":
+                        Size = DoubleElement.ValueOf(child);
+                        break;
+                    case "diffuse":
+                        Diffuse = child.InnerText;
+                        break;
+                    case "normal":
+                        Normal = child.InnerText;
+                        break;
+                }
+            }
+        }
+    }
+}
