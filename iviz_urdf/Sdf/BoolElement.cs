e80b4f32971489d10bbee3c65044fa443a0dc01c
Antonio Zea <antonio.zea@kit.edu>
2020-11-12 14:58:10 +0100

    Adding net 5 to targets

--- iviz_urdf/Sdf/BoolElement.cs
+++ iviz_urdf/Sdf/BoolElement.cs
@@ -6,7 +6,6 @@ namespace Iviz.Sdf
     public static class BoolElement
     {
         internal static bool ValueOf(XmlNode node) => 
-            node is null ? throw new MalformedSdfException() : 
-            (node.InnerText == "1" || node.InnerText == "true");
+            node.InnerText == "1" || node.InnerText == "true";
     }
 }
