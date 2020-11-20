ee7c8a37c60c992f7577569b14bdb9ab8387b0cd
Antonio Zea <antonio.zea@kit.edu>
2020-11-20 08:50:05 +0100

    Added uri parameter

--- iviz_rosmaster/Program.cs
+++ iviz_rosmaster/Program.cs
@@ -9,11 +9,31 @@ namespace Iviz.RosMaster
     {
         static async Task Main(string[] args)
         {
+            Uri masterUri = null;
+            
+            if (args.Length >= 1
+                && args[1] == "--uri")
+            {
+                if (args.Length == 1)
+                {
+                    Console.Error.WriteLine("EE Expected an uri as parameter in position 2");
+                    return;
+                }
+
+                if (!Uri.TryCreate(args[2], UriKind.Absolute, out Uri uri))
+                {
+                    Console.Error.WriteLine("EE Could not parse position 2 as an uri (example: http://XXX:1234)");
+                    return;
+                }
+
+                masterUri = uri;
+            }
+            
             Logger.Log = Console.WriteLine;
             Logger.LogError = Console.Error.WriteLine;
             
-            Uri masterUri = RosClient.TryGetCallerUri(RosMasterServer.DefaultPort);
-            using RosMasterServer rosMasterServer = new RosMasterServer(masterUri);
+            masterUri ??= RosClient.TryGetCallerUri(RosMasterServer.DefaultPort);
+            using RosMasterServer rosMasterServer = new RosMasterServer(masterUri);    
             Console.WriteLine("** Iviz.RosMaster: Starting...");
             rosMasterServer.AddKey("/rosdistro", "noetic");
             rosMasterServer.AddKey("/rosversion", "1.15.8");
