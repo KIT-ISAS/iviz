288b444c04a634d43568b8e56d6d43a41928b097
Antonio Zea <antonio.zea@kit.edu>
2020-11-23 11:21:56 +0100

    Testing scripts for common tasks

---
+++ Bin/Iviz.ModelService.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet $(dirname $BASH_SOURCE)/../iviz_model_service/Publish/Iviz.ModelService.dll $@
---
+++ Bin/Iviz.MsgsGen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet $(dirname $BASH_SOURCE)/../iviz_msgs_gen/Publish/Iviz.MsgsGen.dll $@
