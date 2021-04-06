7ac57b71360c6e1916d8c5a3aec7a9b15059a193
Antonio Zea <antonio.zea@kit.edu>
2021-04-06 22:56:06 +0200

    Adding helper deconstruct

--- iviz/Assets/Core/Utils/UnityUtils.cs
+++ iviz/Assets/Core/Utils/UnityUtils.cs
@@ -606,6 +606,9 @@ namespace Iviz.Core
         public static void Deconstruct(this Vector3 v, out float x, out float y, out float z) =>
             (x, y, z) = (v.x, v.y, v.z);
 
+        public static void Deconstruct(this Vector2 v, out float x, out float y) =>
+            (x, y) = (v.x, v.y);
+
         public static void Deconstruct(this float3 v, out float x, out float y, out float z) =>
             (x, y, z) = (v.x, v.y, v.z);
 
