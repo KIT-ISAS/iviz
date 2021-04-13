7920ff011fcf71a8d5fdc9ec8197f893ee215f25
Antonio Zea <akzeac@gmail.com>
2021-04-13 10:16:16 +0200

    Change language to 9, update msg data reading for new wrappers

--- iviz_msgs/ISerializable.cs
+++ iviz_msgs/ISerializable.cs
@@ -5,7 +5,8 @@ namespace Iviz.Msgs
     /// <summary>
     /// Attribute that tells the Unity Engine not to strip these fields even if no code accesses them.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct |
+                    AttributeTargets.Method | AttributeTargets.Property)]
     public class PreserveAttribute : Attribute
     {
     }
@@ -32,14 +33,14 @@ namespace Iviz.Msgs
         /// Checks if this message is valid. If not, throws an exception.
         /// </summary>
         void RosValidate();
-        
+
         /// <summary>
         /// Creates a new message and deserializes into it the information read from the given buffer.
         /// </summary>
         /// <param name="b">
         /// Buffer object.
         /// </param>
-        ISerializable RosDeserialize(ref Buffer b);        
+        ISerializable RosDeserialize(ref Buffer b);
     }
 
     public interface IDeserializable<out T> where T : ISerializable
