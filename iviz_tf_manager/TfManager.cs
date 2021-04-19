20d459972b90e4ea101a0e577236c8f80414dec6
Antonio Zea <akzeac@gmail.com>
2021-04-19 06:58:35 +0200

    Adding asyncs to tf manager

--- iviz_tf_manager/TfManager.cs
+++ iviz_tf_manager/TfManager.cs
@@ -1,40 +1,77 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Iviz.Msgs.GeometryMsgs;
+using Iviz.Msgs.StdMsgs;
 using Iviz.Msgs.Tf2Msgs;
+using Iviz.Roslib;
 
 namespace Iviz.TfManager
 {
     public class TfManager
     {
-        readonly Dictionary<string, TransformStamped> frames = new();
+        readonly ConcurrentDictionary<string, TransformStamped> frames = new();
 
-        public void Update(in TransformStamped ts)
+        public bool TryGetTransform(string frame, out TransformStamped t) => frames.TryGetValue(frame, out t);
+
+        void Update(in TransformStamped ts)
         {
-            if (ts.ChildFrameId == null)
+            if (string.IsNullOrEmpty(ts.ChildFrameId))
             {
                 return;
             }
 
-            frames[ts.ChildFrameId] = ts;
-        }
+            frames[ts.ChildFrameId!] = ts;
 
-        public void Update(TFMessage t)
-        {
-            foreach (var ts in t.Transforms)
+            string? parentId = ts.Header.FrameId;
+            if (!string.IsNullOrEmpty(parentId) && !frames.ContainsKey(parentId!))
             {
-                Update(ts);
+                frames.TryAdd(parentId!, new TransformStamped
+                {
+                    Header = (0, ts.Header.Stamp, ""),
+                    ChildFrameId = parentId,
+                    Transform = Transform.Identity
+                });
             }
         }
+
+        public void Update(TFMessage tfMessage)
+        {
+            foreach (var transformStamped in tfMessage.Transforms)
+            {
+                Update(transformStamped);
+            }
+        }
+
+        public void UpdateAll(RosChannelReader<TFMessage> reader, CancellationToken token = default)
+        {
+            foreach (var tfMessage in reader.ReadAll(token))
+            {
+                Update(tfMessage);
+            }
+        }
+
+
+#if !NETSTANDARD2_0
+        public async Task UpdateAllAsync(RosChannelReader<TFMessage> reader, CancellationToken token = default)
+        {
+            await foreach (var tfMessage in reader.ReadAllAsync(token))
+            {
+                Update(tfMessage);
+            }
+        }
+#endif
 
         bool TryGetTransformToRoot(string frame, out (Transform transform, string root) t)
         {
             t.root = frame ?? throw new ArgumentNullException(nameof(frame));
             t.transform = Transform.Identity;
-            
-            while (frames.TryGetValue(t.root, out TransformStamped ts) && !string.IsNullOrEmpty(ts.Header.FrameId))
+
+            while (frames.TryGetValue(t.root!, out TransformStamped ts) && !string.IsNullOrEmpty(ts.Header.FrameId))
             {
-                t.root = ts.Header.FrameId;
+                t.root = ts.Header.FrameId!;
                 t.transform = ts.Transform * t.transform;
             }
 
@@ -47,7 +84,7 @@ namespace Iviz.TfManager
                 !TryGetTransformToRoot(toFrame, out var to) ||
                 to.root != from.root)
             {
-                transform = Transform.Identity;
+                transform = default;
                 return false;
             }
 
