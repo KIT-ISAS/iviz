95ceb95322ead023a5b914aec5d969a9d050bcb8
Antonio Zea <antonio.zea@kit.edu>
2021-05-30 11:20:23 +0200

    Adding generic ros connection interface

--- iviz_msgs/IRosConnection.cs
+++ iviz_msgs/IRosConnection.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Iviz.Msgs
 {
     /// <summary>
@@ -9,13 +11,44 @@ namespace Iviz.Msgs
         /// The connection topic
         /// </summary>
         string Topic { get; }
+
         /// <summary>
         /// The ROS message type
         /// </summary>
         string Type { get; }
+
         /// <summary>
         /// List of TCPROS header entries in the form of strings a=b
         /// </summary>
-        string[] TcpHeader { get; }
+        IReadOnlyCollection<string> TcpHeader { get; }
+    }
+
+    public static class RosConnection
+    {
+        public static IRosConnection Create<T>(string topic, string callerId, bool requestNoDelay = false)
+            where T : IMessage
+        {
+            string type = BuiltIns.GetMessageType<T>();
+            return new RosConnectionImpl(topic, type,
+                new[]
+                {
+                    $"message_definition={BuiltIns.DecompressDependencies<T>()}",
+                    $"callerid={callerId}",
+                    $"topic={topic}",
+                    $"md5sum={BuiltIns.GetMd5Sum<T>()}",
+                    $"type={type}",
+                    requestNoDelay ? "tcp_nodelay=1" : "tcp_nodelay=0"
+                });
+        }
+
+        class RosConnectionImpl : IRosConnection
+        {
+            public string Topic { get; }
+            public string Type { get; }
+            public IReadOnlyCollection<string> TcpHeader { get; }
+
+            public RosConnectionImpl(string topic, string type, IReadOnlyCollection<string> tcpHeader) =>
+                (Topic, Type, TcpHeader) = (topic, type, tcpHeader);
+        }
     }
 }
